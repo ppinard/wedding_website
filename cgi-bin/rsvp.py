6a5037fd69dbc0ce670581bf993706721b41449d
Philippe Pinard <philippe.pinard@gmail.com>
2017-02-04 18:00:15 +0000

    Update cgi script

--- cgi-bin/rsvp.py
+++ cgi-bin/rsvp.py
@@ -1,35 +1,57 @@
+#!/usr/local/bin/python3
+
 import cgi
 import smtplib
+import csv
+import json
+from email.mime.text import MIMEText
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 form = cgi.FieldStorage()
 name =  form.getvalue('name')
-lastname =  form.getvalue('lastname')
 email =  form.getvalue('email')
-message =  form.getvalue('message')
+attendance =  form.getvalue('attendance')
+guest =  form.getvalue('guest')
+allergies =  form.getvalue('allergies')
+recaptcha = form.getvalue('g-recaptcha-response')
 
+url = 'https://www.google.com/recaptcha/api/siteverify'
+post_fields = {'secret': '6Ld9WBQUAAAAAFnQ__rDorWS0UEcGI7ml6BZg9xN',
+               'response': recaptcha}
+request = Request(url, urlencode(post_fields).encode())
+response = urlopen(request).read().decode()
+content = json.loads(response)
 
-#SERVER = "localhost"
+if not content['success']:
+    print("Content-Type: text/html")
+    print()
+    print("<title>Sepideh &amp; Philippe</title>")
+    print("<h1>Invalid</h1>")
+    return
 
-FROM = 'philippe.pinard@gmail.com'
+with open('rsvp.csv', 'a') as fp:
+    writer = csv.writer(fp)
+    writer.writerow([name, email, attendance, guest, allergies])
 
-TO = ["sebrahimi1988@gmail.com"] # must be a list
+from_addr = 'philippe@sepidehphilippe.com'
+to_addrs = ['philippe.pinard@gmail.com', 'sebrahimi1988@gmail.com']
+text = 'name: {}\nemail: {}\nattendance: {}\nguest: {}\nallergies: {}' \
+    .format(name, email, attendance, guest, allergies)
+msg = MIMEText(text)
+msg['Subject'] = 'Wedding RSVP'
+msg['From'] = from_addr
+msg['To'] = ';'.join(to_addrs)
 
-SUBJECT = "New wedding guest"
+s = smtplib.SMTP()
+s.connect('smtp.webfaction.com')
+s.login('sepidehphilippe','ahprETDzDe2Huk3ekH4j')
+s.sendmail(from_addr, to_addrs, msg.as_string())
+s.quit()
 
-TEXT = 'name: {}, lastname: {}, email: {}, allergies: {}'.format('name', 'lastname', 'email', 'message')
+print("Content-Type: text/html")
+print()
+print("<title>Sepideh &amp; Philippe</title>")
+print("<h1>RSVP</h1>")
+print("<p>We have received your information. Thank you</p>")
 
-# Prepare actual message
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-# Send the mail
-
-server = smtplib.SMTP('myserver')
-server.sendmail(FROM, TO, message)
-server.quit()
